ba6e73f41324ef49d5846bfc052351cb2b386720
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-02-23 09:54:00 -0700

    css for result

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,4 +2,9 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  // $('#submit_form').on('click', function(){
+  //   ('.results').append("<p><a href=https://galvanize-eats.herokuapp.com/ class=icon bar project-links><i class=fa fa-globe></i></a></p>")
+  // })
+
 });
