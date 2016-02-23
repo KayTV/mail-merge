50332bb21b0fe4f3eca5609ee178efa9d9f19735
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-02-22 19:22:21 -0700

    completed project

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mail Merge' });
 });
 
+router.post('/', function(req, res, next){
+  var subject = req.body.Subject;
+  var body = req.body.Body;
+  var email = req.body.Emails;
+  var splitEmail = email.split('\r\n');
+  for(var i = 0; i < splitEmail.length; i++) {
+    splitEmail[i] = splitEmail[i].split(',');
+  }
+  console.log(splitEmail)
+  var renderObj = {
+    emails: splitEmail,
+    body: body,
+    subject: subject,
+    title: 'Mail Merge'
+  };
+  console.log(renderObj);
+  res.render('index', renderObj);
+})
+
 module.exports = router;
+
+//when you hit a new line, that means a new array
+//somehow put it in a loop to get each email address
+//once those parts are good, each one will appear in a content box in HTML
+//with once on click
+//define object within the loop, for in loop?
